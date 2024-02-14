39c132c7912676d000b4c9701dfd53a3a8759ac6
sahu391 <sahanagowda391@gmail.com>
2024-02-14 15:56:57 +0530

    Worked on routes prototype

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import navs from "./Component/Routes/Navigation.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+const user = {
+  username: "",
+  role: "ALL",
+  isAuthenticated: false,
+};
+
+const {role,isAuthenticated}=user;
+
+const allRoutes = () => {
+  return (
+    <Route path={"/"} element={<App/>}>
+      {navs.map((nav,i) => {
+        if(isAuthenticated){
+          if(nav.isVisibleAfterAuth){
+            if(nav.role === role || nav.role === "ALL"){
+              console.log(nav)
+              return <Route key={i} path={nav.path} element={nav.element}/>
+            }
+          }
+        }else{
+            if(!nav.requireAuth && nav.role==="ALL"){
+              console.log(nav)
+              return <Route key={i} path={nav.path} element={nav.element}/>
+            }
+          }
+        
+
+      })}
+    </Route>
+  )
+}
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Routes>
+        <Route path={"/"} element={<App />} />
+        {allRoutes()}
+      </Routes>
     </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
